66d885d51c7d34d50d2940d989733c5ee871889f
Cristian Cruz <criscruz1806@gmail.com>
2019-10-03 17:40:53 -0500

    Inicio primer formulario de registro

--- public/js/app.js
+++ public/js/app.js
@@ -79583,8 +79583,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Documents\Proyectos\mediclic\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Documents\Proyectos\mediclic\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Desarrollo\PhpstormProjects\mediclic\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Desarrollo\PhpstormProjects\mediclic\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-mediclic">
+<div class="container-fluid">
+
+    <!--Inicio de sesión-->
     <div class="row img-header">
         <div class="col-md-12 col-padding-header">
             <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
+               @csrf
                 <div class="row padding-input-login">
                     <div class="col-md-2 offset-6">
                         <input id="email" type="email" class="form-control form-control-sm @error('email') is-invalid @enderror" aria-label="Small" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Correo electrónico">
-
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-{{--                        <img src="/img/login/recordarcontrasenia.png" alt="Responsive image" class="img-forgot">--}}
-
-{{--                        @if (Route::has('password.request'))--}}
-{{--                            <a class="btn-forgot" href="{{ route('password.request') }}">--}}
-{{--                                ¿Olvidaste tu contraseña?--}}
-{{--                            </a>--}}
-{{--                        @endif--}}
                     </div>
 
                     <div class="col-md-2">
@@ -30,8 +18,8 @@
 
                         @error('password')
                         <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                            <strong>{{ $message }}</strong>
+                        </span>
                         @enderror
                     </div>
 
@@ -41,6 +29,19 @@
                         </button>
                     </div>
                 </div>
+
+                @if ($errors->any())
+                    <div class="row padding-input-login">
+                        <div class="col-md-4 offset-6">
+                            @error('email')
+                            <span class="invalid-feedback d-block" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                @endif
+
                 <div class="row padding-input-login">
                     <div class="col-md-2 offset-6">
                         <img src="/img/login/recordarcontrasenia.png" alt="Responsive image" class="img-forgot">
@@ -52,8 +53,35 @@
                         @endif
                     </div>
                 </div>
-                    </form>
+            </form>
         </div>
+    </div>
+
+    <!--Imagen de inicio y formulario de registro-->
+    <div class="row">
+        <!--Imagen de inicio-->
+        <div class="col-md-5 img-inicio"></div>
+
+        <!--Formulario de registro paciente-->
+        <div class="col-md-7">
+            <div class="row">
+                <div class="offset-3 col-md-6 img-logo-inicio"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="h1-registro">Registro</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Franja de colores footer-->
+    <div class="row color-footer">
+        <div class="col-md-4 color-footer-1"></div>
+        <div class="col-md-2 color-footer-2"></div>
+        <div class="col-md-2 color-footer-3"></div>
+        <div class="col-md-2 color-footer-4"></div>
+        <div class="col-md-2 color-footer-5"></div>
     </div>
 </div>
 @endsection
